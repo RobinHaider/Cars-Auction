88d995b5a0ac59d154d44b001d742f0e7116e6b4
RobinHaider <robinhaider69@gmail.com>
2025-11-20 21:28:28 +0600

    Add AuctionsController with GET endpoints
    - Implement GetAllAuctions endpoint returning all auctions ordered by make
    - Implement GetAuctionById endpoint for retrieving specific auctions
    - Remove empty Controllers folder placeholder from csproj
    - Add .editorconfig for consistent code styling (underscore prefix for private fields)

---
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -0,0 +1,50 @@
+using AuctionService.Data;
+using AuctionService.DTOs;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionService.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuctionsController : ControllerBase
+{
+    private readonly AuctionDbContext _context;
+    private readonly IMapper _mapper;
+
+    public AuctionsController(AuctionDbContext context, IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<AuctionDto>>> GetAllAuctions()
+    {
+        var auctions = await _context.Auctions
+            .Include(a => a.Item)
+            .OrderBy(a => a.Item.Make)
+            .ToListAsync();
+
+        return _mapper.Map<List<AuctionDto>>(auctions);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AuctionDto>> GetAuctionById(Guid id)
+    {
+        var auction = await _context.Auctions
+            .Include(a => a.Item)
+            .FirstOrDefaultAsync(a => a.Id == id);
+
+        if (auction == null)
+        {
+            return NotFound();
+        }
+
+        return _mapper.Map<AuctionDto>(auction);
+    }
+
+
+}
