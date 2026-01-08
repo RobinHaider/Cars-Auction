be296b7b93858745292fb9541e6965e8cb2fd61f
RobinHaider <robinhaider69@gmail.com>
2026-01-08 21:28:21 +0600

    Add SearchController with search functionality for items

---
+++ src/SearchService/Controllers/SearchController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Entities;
+using SearchService.Models;
+
+namespace SearchService.Controllers;
+
+[Route("api/search")]
+[ApiController]
+public class SearchController : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<List<Item>>> SearchItem([FromQuery] string searchTerm)
+    {
+        var query = DB.Find<Item>();
+
+        query.Sort(x => x.Ascending(a => a.Make));
+
+        if (!string.IsNullOrEmpty(searchTerm))
+        {
+            query.Match(Search.Full, searchTerm).SortByTextScore();
+        }
+
+        var result = await query.ExecuteAsync();
+
+        return result;
+    }
+}
