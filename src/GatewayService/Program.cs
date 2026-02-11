e7b46862c83cf165d61a5f2477c760ee381fda40
RobinHaider <robinhaider69@gmail.com>
2026-02-11 12:44:25 +0600

    Implement JWT authentication and update reverse proxy configuration in GatewayService

--- src/GatewayService/Program.cs
+++ src/GatewayService/Program.cs
@@ -1,12 +1,28 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // add reverse proxy to the service collection
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
+// Configure JWT Bearer Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = builder.Configuration["IdentityServiceUrl"];
+        options.RequireHttpsMetadata = false;
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.NameClaimType = "username";
+    });
+
 var app = builder.Build();
 
 // add reverse proxy to the request pipeline
 app.MapReverseProxy();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
 
 app.Run();
